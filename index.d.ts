2ed9289286868b0823d0718fef77e963b7d4438b
Ítalo Nascimento <italo.abnascimento@gmail.com>
2018-09-11 17:07:18 -0300

    specify return types in .d.ts

--- index.d.ts
+++ index.d.ts
@@ -1,4 +1,11 @@
 declare module "@swp/wallet-sdk" {
+  type OperationType =
+    "payment" |
+    "create_account" |
+    "create_organization" |
+    "issue_asset" |
+    "change_trust"
+
   interface Options {
     apiKey: string
     secret: string
@@ -6,27 +13,89 @@ declare module "@swp/wallet-sdk" {
     debug: boolean
   }
 
-  interface Payment {
+  interface PaymentOp {
     from: string
     to: string
     asset: string
     amount: number
   }
 
+  interface Payment {
+    id: string
+    payment_ops: PaymentOp[]
+  }
+
+  interface Receipt {
+    id: string
+    created_at: string
+    op_type: OperationType
+  }
+
+  interface Balance {
+    balance: number
+    asset_code: string
+    asset_id: string
+  }
+
+  interface Account {
+    id: string
+    balances: Balance[]
+  }
+
+  interface Asset {
+    id: string
+    code: string
+    limit: number
+  }
+
+  interface Organization extends Account {
+    name: string
+    api_key: string
+    secret: string
+  }
+
+  interface WithReceipt {
+    receipt: Receipt
+  }
+
+  interface AccountReceipt extends WithReceipt {
+    account: Account
+  }
+
+  interface AssetReceipt extends WithReceipt {
+    asset: Asset
+  }
+
+  interface OrganizationReceipt extends WithReceipt {
+    organization: Organization
+  }
+
+  interface PaymentReceipt extends WithReceipt {
+    payment: Payment
+  }
+
   interface Endpoints {
-    createAccount: () => Promise<any>
-    getAccount: (id: string) => Promise<any>
-    getAllAccounts: () => Promise<any>
-    getAssets: () => Promise<any>
-    getOrganization: () => Promise<any>
-    makePayment: (payments: Array<Payment>) => Promise<any>
-    monitorPaymentsToAccount: (id: string, cb: (payment: Payment) => void) => EventSource
-    monitorPaymentsToOrg: (cb: (payment: Payment) => void) => EventSource
+    createAccount: () => Promise<AccountReceipt>
+    getAccount: (id: string) => Promise<AccountReceipt>
+    getAllAccounts: () => Promise<AccountReceipt[]>
+    getAssets: () => Promise<AssetReceipt[]>
+    getOrganization: () => Promise<OrganizationReceipt>
+    makePayment: (payments: Array<PaymentOp>) => Promise<PaymentReceipt>
+    monitorPaymentsToAccount: (id: string, cb: (payment: PaymentReceipt) => void) => EventSource
+    monitorPaymentsToOrg: (cb: (payment: PaymentReceipt) => void) => EventSource
   }
   
   export const languages: {
     PT_BR: string
     EN_US: string
+  }
+
+  export const operationTypes: {
+    Payment: "payment",
+    CreateAccount: "create_account",
+    CreateOrganization: "create_organization",
+    IssueAsset: "issue_asset",
+    ChangeTrust: "change_trust",
   }
 
   export const init: (options: Options) => Endpoints
